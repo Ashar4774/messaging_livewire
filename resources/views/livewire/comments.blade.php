9f7c44e5163599fd64cbc4d0eec8a9d7a3f79276
ashar4774 <ashar.muhammad74@gmail.com>
2022-12-16 03:31:40 +0500

    Added 1st commit, already created livewire 1st component named as Comments

---
+++ resources/views/livewire/comments.blade.php
@@ -0,0 +1,20 @@
+<div class="flex justify-center">
+    <div class="w-6/12">
+        <h1 class="my-10 text-3x1">Comments</h1>
+        <div class="my-4 flex">
+            <input type="text" name="" id="" class="w-full rounded border shadow p-2 mr-2 my-2" placeholder="What's in your mind?">
+            <div class="py-2">
+                <button class="p-2 bg-blue-500 w-20 rounded shadow text-white">Add</button>
+            </div>
+        </div>
+        <div class="rounded border shadow p-3 my-2">
+            <div class="justify-start flex my-2">
+                <p class="font-bold text-lg">Sarthak</p>
+                <p class="mx-3 py-1 text-xs text-gray-500 font-semibold">3 min ago</p>
+            </div>
+            <p class="text-gray-800">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore veniam quidem tenetur est suscipit quo, commodi laudantium provident doloremque, perferendis sapiente et id officiis libero, quos rerum mollitia eligendi aliquam.
+            </p>
+        </div>
+    </div>
+</div>
